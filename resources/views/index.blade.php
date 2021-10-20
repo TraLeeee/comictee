060f06ae93310869aecb78500257abdcdcc01fe1
Trà Lê <chexanh1998@gmail.com>
2021-10-20 17:32:31 +0700

    Remove tailwindcss from source cause it's complicated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,14 +7,26 @@
     <title>ComicTee</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-<div class="card">
-    <button class="btn">ring-0</button>
-    <button class="btn">ring-2</button>
-    <button class="btn">ring</button>
-    <button class="btn">ring-4</button>
-</div>
-
+<body class="main-bg">
+<main>
+    <header class="container header">
+        <div class="logo">
+            <img src="https://laravel.com/img/logomark.min.svg" alt="Logo">
+        </div>
+        <div class="search">
+            <form action="#" method="get" class="search-form">
+                <input type="text" class="search-input" placeholder="Search...">
+                <button type="submit" class="search-button" value="Submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                         class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
+                </button>
+            </form>
+        </div>
+    </header>
+</main>
 <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
